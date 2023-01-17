5a6c1effbf8611241bd2440fed20cc8bff5b0124
ManueleVeggi <manuele.veggi@studio.unibo.it>
2023-01-17 12:15:53 +0100

    first draft website complete

--- README.md
+++ README.md
@@ -12,8 +12,47 @@ Main site at the following [link](https://manueleveggi.github.io/infoviz/)
 
 ## References
 
-- MET Source dataset available [here](https://github.com/metmuseum/openaccess/blob/master/MetObjects.csv)
-- Website template available [here](https://github.com/BuckyMaler/global)
+### Source datasets
+
+Two main datasets have beel used
+
+- The Metropolitan Museum of Art Open Access CSV, [link](https://github.com/metmuseum/openaccess/blob/master/MetObjects.csv)
+- Zeri&LODE RDF Datasets, [link](http://data.fondazionezeri.unibo.it/)
+
+### Bibliography and online resources
+
+The main references publications for the website are the catalog written by Zeri for the collection of Italian painting at MET:
+
+Zeri Z. and E. Gardner. <i>Italian Paintings. A Catalogue of the Collection of
+The Metropolitan Museum of Art</i>. Vicenza: Neri Pozza.
+
+| Volume no. | Title                              | Year | Online resource                                                                                                                                              |
+|------------|------------------------------------|------|--------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| 1          | Florentine School                  | 1971 | [Link](https://www.metmuseum.org/art/metpublications/italian_paintings_a_catalogue_of_the_collection_of_the_metropolitan_museum_of_art_vol_1_florentin)      |
+| 2          | Venetian School                    | 1973 | [Link](http://www.metmuseum.org/art/metpublications/italian_paintings_a_catalogue_of_the_collection_of_the_metropolitan_museum_of_art_vol_2_venetian_school) |
+| 3          | Sienese and Central Italian School | 1986 | [Link](https://www.metmuseum.org/art/metpublications/italian_paintings_a_catalogue_of_the_collection_of_the_metropolitan_museum_of_art_vol_3_sienese_a)      |
+| 4          | North Italian School               | 1986 | [Link](https://www.metmuseum.org/art/metpublications/Italian_Paintings_A_Catalogue_of_the_Collection_of_The_Metropolitan_Museum_of_Art_Vol_4_North_Ita)      |
+
+Other relevant resources have been:
+
+- <i>Thomas J. Watson Library Digital Collections</i>, The Metropolitan Museum of Art ([link](https://www.metmuseum.org/art/libraries-and-research-centers/watson-digital-collections))
+- <i>Grange-Blanche, Louise Labé et Pierre-Toussaint de Chazelles</i>, website of the city of Parcieux ([link](https://www.parcieux.fr/Grange-Blanche-Louise-Labe-et-Pierre-Toussaint-de-Chazelles.html?lang=fr))
+- Cheney, Iris H. <i>Notes on Jacopino del Conte</i>. "Art Bulletin" 52 (March 1970), 1970.
+- Corso, M. <i>Jacopino del Conte nel contesto artistico romano tra gli anni trenta e gli anni cinquanta del Cinquecento</i>. PhD diss., Università degli Studi Roma Tre, 2014.
+- Privitera, M. <i>Girolamo Macchietti : un pittore dello Studiolo di Francesco 1. (Firenze 1535-1592)</i>. Milan: Jandi Sapi editori, 1966.
+- Salinger, M. <i>A Portrait of a Knight of Malta</i>. "The Metropolitan Museum of Art Bulletin", vol. 2, no. 5, 1944, pp. 164–65. ([JSTOR](https://doi.org/10.2307/3257124)).
+- Vasari, G. <i>Le vite dei più eccellenti pittori, scultori e architetti</i>. Guttemberg, 1876 ([link](https://books.google.it/books?hl=it&lr=&id=97PcyE3ksa4C&oi=fnd&pg=PA3&dq=vasari+vite&ots=m6DvwRDKtZ&sig=4-wbsqnw-NrFU_dCCeNVQzTeEz8&redir_esc=y#v=onepage&q=vasari%20vite&f=false))
+
+As the historical and biographical reconstructions of main European and US art dealers is still ongoing by scholars, not always bibliographical resources were founded. The main pieces of information about the dealers are hence coming from the online articles published by the Metropolitan Museum of Art and, in the worst scenarios, from online encyclopedias (Wikipedia). 
+
+### Templates and libraries
+
+Used website template is available [here](https://github.com/BuckyMaler/global) and the layout was edited through [Bootstrap v 5.0.2](https://getbootstrap.com/).
+
+Visualizations and multimedia content presentations are created through the following libraries:
+- [Google Charts](https://developers.google.com/chart)
+- [Leaflet](https://leafletjs.com/)
+- [StoryMap - KnightLab](https://storymap.knightlab.com/)
 
 ## License
 
--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,59 +1,130 @@
-function toggleDiv(className, id) { 
+function toggleDiv(className, id) {
     var myClass = document.getElementsByClassName(className);
-    for(var i = 0; i < myClass.length; i++) { 
-        myClass[i].style.display='none'
+    for (var i = 0; i < myClass.length; i++) {
+        myClass[i].style.display = 'none'
     }
-    
+
     var toDisplay = document.getElementById(id);
 
-    if (id=='finalmain') {
+    /*
+    if (id == 'finalmain') {
         location.reload
-        toDisplay.style.marginTop = "15%";
-        toDisplay.style.transform = "translateY(-4rem)";
+        toDisplay.style.marginTop = "8%";
+        toDisplay.style.transform = "translateY(-2rem)";
     }
+    */
 
-    toDisplay.style.display = "block"; 
+    toDisplay.style.display = "block";
 
 }
 
 function toggleDivViz(className, id) {
     var myClass = document.getElementsByClassName(className);
-    for(var i = 0; i < myClass.length; i++) { 
-        myClass[i].style.display='none';
+    for (var i = 0; i < myClass.length; i++) {
+        myClass[i].style.display = 'none';
         myClass[i].classList.remove("replicateTransition");
     }
-    
+
     var toDisplay = document.getElementById(id);
     if (id.includes("row")) {
         toDisplay.style.display = "flex"
-    }
-    else {
-        toDisplay.style.display = "block"; 
+    } else {
+        toDisplay.style.display = "block";
     }
 
-    document.getElementById('vizheader').style.display = "none"; 
-    document.getElementById('metfacade').style.display = "none"; 
-    
+    document.getElementById('vizheader').style.display = "none";
+    document.getElementById('metfacade').style.display = "none";
+
     toDisplay.classList.add("replicateTransition")
 
 }
 
 function toggleScrJS(id) {
     var myClass = document.getElementsByClassName("myjsgooglechart");
-    for(var i = 0; i < myClass.length; i++) { 
-        myClass[i].src='';
+    for (var i = 0; i < myClass.length; i++) {
+        myClass[i].src = '';
     }
 
     if (id == "jschartartist") {
-        var jspath =  "assets/js/googlechart/artistgraph.js"
+        var jspath = "assets/js/googlechart/artistgraph.js"
     } else if (id == "jschartdate") {
-        var jspath =  "assets/js/googlechart/accessionline.js"
+        var jspath = "assets/js/googlechart/accessionline.js"
     } else if (id == "jscharttype") {
-        var jspath =  "assets/js/googlechart/accessiontype.js"
+        var jspath = "assets/js/googlechart/accessiontype.js"
     } else if (id == "jschartdealer") {
-        var jspath =  "assets/js/googlechart/dealergraph.js"
+        var jspath = "assets/js/googlechart/dealergraph.js"
     }
 
     var toDisplay = document.getElementById(id);
     toDisplay.src = jspath;
+}
+
+var map = null;
+
+function checkMap() {
+    
+}
+
+function LeafletShowImg(divId, filepath) {
+    /*
+    if (filepath == 'letter_pag1') {
+        var filepath = "assets/zeriLetters/digitalization/1949_10_13_a.jpg";
+    } else if (filepath == 'letter_pag2') {
+        var filepath = "assets/zeriLetters/digitalization/1949_10_13_b.jpg";
+    }
+    */
+
+    var filepath = "assets/zeriLetters/digitalization/1949_10_13_a.jpg";
+
+    var map = L.map(divId, {
+        minZoom: 0,
+        maxZoom: 4,
+        center: [0, 0],
+        zoom: 0.5,
+        crs: L.CRS.Simple
+    });
+
+    // dimensions of the image
+    var w = 2344,
+        h = 3180,
+        url = filepath;
+
+    // calculate the edges of the image, in coordinate space
+    var southWest = map.unproject([0, h], map.getMaxZoom() - 1);
+    var northEast = map.unproject([w, 0], map.getMaxZoom() - 1);
+    var bounds = new L.LatLngBounds(southWest, northEast);
+
+    // add the image overlay, 
+    // so that it covers the entire map
+    L.imageOverlay(url, bounds).addTo(map);
+
+    // tell leaflet that the map is exactly as big as the image
+    map.setMaxBounds(bounds);
+
+}
+
+function showSectionProgress(className, sectionName, progressLabelNo, progressWidth) {
+
+    // Display
+    var myClass = document.getElementsByClassName(className);
+    for (var i = 0; i < myClass.length; i++) {
+        myClass[i].style.display = 'none'
+    }
+
+    document.getElementById(sectionName).style.display = "flex";
+
+    // Bar
+    document.getElementById('zeribar').style.width = progressWidth;
+
+    // Color label
+    var myBarLabel = document.getElementsByClassName("zeriProgressLabel");
+
+    for (var i = 0; i < myBarLabel.length; i++) {
+        if (i < myBarLabel.length - (myBarLabel.length - progressLabelNo)) {
+            myBarLabel[i].style.color = "#ed0025";
+        } else {
+            myBarLabel[i].style.color = 'gray';
+        }
+    }
+
 }
